b75b21f36815fc109108c74f6aee2d8681b8201b
SAhmed28 <sanjida1@ualberta.ca>
2022-11-28 05:32:27 -0600

    usersCollection storing data

--- index.js
+++ index.js
@@ -13,9 +13,20 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oplybtq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run(){
     try{
+        const usersCollection = client.db('smartTech').collection('users');
 
+        
+
+        app.post('/users', async (req,res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
     }
     finally{
 
